a284f840d3aca90380725ddee609b92f32187783
oiKeTTLe <87470516+oiKeTTLe@users.noreply.github.com>
2025-01-02 21:39:05 +1100

    Added Player Sprite

--- game/Scripts/Player/player.gd
+++ game/Scripts/Player/player.gd
@@ -3,12 +3,11 @@ extends CharacterBody2D
 var max_speed : float = 200
 var acceleration : float = 3000
 
-var axis : Vector2
-
 @export var cam_gradient : float = 1000
 @export var max_cam_offset : float = 64
 
 @onready var camera = $player_cam
+@onready var player_animator = $AnimatedSprite2D
 
 func apply_friction(amount):
 	if velocity.length() > amount:
@@ -21,6 +20,7 @@ func apply_movement(acceleration):
 	velocity = velocity.limit_length(max_speed)
 
 func get_dir():
+	var axis : Vector2
 	axis.x = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	axis.y = int(Input.is_action_pressed("move_down")) - int(Input.is_action_pressed("move_up"))
 	return axis.normalized()
@@ -29,13 +29,19 @@ func _physics_process(delta: float) -> void:
 	var dir = get_dir()
 	if dir == Vector2.ZERO:
 		apply_friction(acceleration * delta)
+		player_animator.play("idle")
 	else:
 		apply_movement(dir * acceleration * delta)
+		player_animator.play("run")
 	
 	#$animations.look_at(get_global_mouse_position())
 	
 	move_and_slide()
 
 func _process(delta: float) -> void:
+	player_animator.flip_h = true if velocity.x < 0 else false
+	if get_dir() == Vector2.ZERO:
+		player_animator.flip_h = true if get_global_mouse_position().x < global_position.x else false
+	
 	var mouseoffset = (get_viewport().get_mouse_position() - get_viewport_rect().size / 2)
 	camera.position = lerp(Vector2.ZERO, mouseoffset.normalized() * max_cam_offset, mouseoffset.length() / cam_gradient)
